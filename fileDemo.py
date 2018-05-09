142e33e9d34015573112f59e41332df617b523f3
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-05-09 08:39:07 -0400

    Updated from Brython Server: 5/9/2018 8:39:06 AM

--- fileDemo.py
+++ fileDemo.py
@@ -1,1 +1,13 @@
+#juliagolder
+#5/9/18
+#fileDemo.py
 
+file = open('engmix.txt')
+
+numWords = 0
+for line in file:
+    if 'julia' in line:
+        print(line.strip())
+    numWords += 1
+    
+print(numWords)
