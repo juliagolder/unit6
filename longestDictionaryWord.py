343ecb30d0b7d017843ac268655f532e5bd22003
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-05-10 08:14:34 -0400

    Updated from Brython Server: 5/10/2018 8:14:36 AM

--- longestDictionaryWord.py
+++ longestDictionaryWord.py
@@ -1,1 +1,16 @@
+#juliagolder
+#5/10/18
+#longestDictionaryWord.py
 
+file = open('engmix.txt')
+
+longest = 0
+LONGword = ''
+
+
+for item in file:
+    if longest < (len(item)):
+        longest = (len(item))
+        LONGword = item
+
+print(LONGword)
