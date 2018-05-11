de6ad829c43001d164f976ed9dcc80ec3a28d8fb
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-05-11 08:31:03 -0400

    Updated from Brython Server: 5/11/2018 8:31:04 AM

--- warmup16.py
+++ warmup16.py
@@ -1,1 +1,11 @@
+#juliagolder
+#5/11/18
+#warmup16.py
 
+file = open('engmix.txt')
+
+
+for line in file:
+    line = line.strip()
+    if len(line) > 0 and line[0]=='j' and line[-1]=='g':
+        print(line)
