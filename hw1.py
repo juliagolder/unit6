5493fdb5801b70f7e124c61a1c858a7df48e99a1
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-05-14 08:14:09 -0400

    Updated from Brython Server: 5/14/2018 8:14:09 AM

--- hw1.py
+++ hw1.py
@@ -2,4 +2,14 @@
 #5/14/18
 #hw1.py
 
+file = open('engmix.txt')
 
+word = input('Enter some words: ')
+
+numWords = 0
+for line in file:
+    if 'julia' in line:
+        print(line.strip())
+    numWords += 1
+
+print
