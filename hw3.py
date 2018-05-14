5564d4ba035819fa0f0a65e7e208d33f4b69713d
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-05-14 08:47:59 -0400

    Updated from Brython Server: 5/14/2018 8:47:59 AM

--- hw3.py
+++ hw3.py
@@ -1,3 +1,13 @@
 #julia golder
 #5/14/18
 #hw3.py
+
+file = open('hw2.py')
+
+for line in file:
+    many[len(line)] += 1
+print(many)
+
+
+for i in range(1,24):
+    print(i,'- letter words: ', many[i-1])
