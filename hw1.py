8f89bf88415945897f79f6046c4fba20f9e03c46
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-05-14 08:14:48 -0400

    Updated from Brython Server: 5/14/2018 8:14:48 AM

--- hw1.py
+++ hw1.py
@@ -4,11 +4,11 @@
 
 file = open('engmix.txt')
 
-word = input('Enter some words: ')
+word = input("Enter the word you're looking for: ")
 
 numWords = 0
 for line in file:
-    if 'julia' in line:
+    if word in line:
         print(line.strip())
     numWords += 1
 
