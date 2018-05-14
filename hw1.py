7c723f2f6230bc9666945dd7ae0e0649819083d7
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-05-14 08:19:32 -0400

    Updated from Brython Server: 5/14/2018 8:19:32 AM

--- hw1.py
+++ hw1.py
@@ -6,12 +6,7 @@ file = open('engmix.txt')
 
 word = input("Enter the word you're looking for: ")
 
-numWords = 0
 for line in file:
     if word in line:
         print(line.strip('This word is in the dictionary'))
-    else:
-        print('This is not in the dictionary')
-    numWords += 1
-
-print
+    
