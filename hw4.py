c8e3456326d74acce0036aa7126879535ef6bb24
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-05-15 08:13:34 -0400

    Updated from Brython Server: 5/15/2018 8:13:35 AM

--- hw4.py
+++ hw4.py
@@ -2,4 +2,16 @@
 #5/14/18
 #hw4.py
 
+file = open('engmix.txt')
 
+letter = input('What letter are you looking for?')
+
+most = 0
+LETTERword = ''
+
+for item in file:
+    if most < (LETTERword.count):
+        most = (LETTERword.count)
+        LETTERword = item
+
+print(LETTERword)
