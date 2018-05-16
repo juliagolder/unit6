36318551d225c5980d862484183e7de4d6dec396
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-05-16 08:31:21 -0400

    Updated from Brython Server: 5/16/2018 8:31:22 AM

--- revsereFile.py
+++ revsereFile.py
@@ -1,1 +1,14 @@
+#juliagolder
+#5/16/18
+#reverseFile.py
 
+THEfile = input('What file would you like to reverse?')
+
+file = open('THEfile')
+
+L = []
+
+for line in file:
+    L.append(line.reverse())
+
+print(L[number-1])
