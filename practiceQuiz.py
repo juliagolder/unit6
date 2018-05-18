cc3d6d7e872bb7267927ba646b9f4e2fa6fad610
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-05-18 08:43:38 -0400

    Updated from Brython Server: 5/18/2018 8:43:39 AM

--- practiceQuiz.py
+++ practiceQuiz.py
@@ -2,6 +2,9 @@
 #5/18/18
 #practiceQuiz.py
 
+#1 find all word in dic. that have exactly 3 c's and 2 p's
+
+#2 - how many words start with the letter r. and aware of blank line in dictionary
 file = open('engmix.txt')
 
 numWords = 0
