6ea947f8fa22cdabd1932025674f011a4d6fef8d
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-05-21 08:46:00 -0400

    Updated from Brython Server: 5/21/2018 8:46:01 AM

--- practiceQuiz.py
+++ practiceQuiz.py
@@ -1,7 +1,7 @@
 #juliagolder
 #5/21/18
 #quiz5.py
-
+"""
 #program1
 file = open('engmix.txt')
 
@@ -13,7 +13,7 @@ for line in file:
     if number == 4:
         print(word)
 
-"""
+#program2
 file = open('engmix.txt')
 
 letter = 't'
@@ -22,8 +22,9 @@ for line in file:
     line = line.strip()
     if line[0] == letter and line[4] == letter and line[8] == letter:
         print(line)
+"""
 
-
+#program3
 file = open('engmix.txt')
 
 letter = input('What letter would you like to search for?')
@@ -33,7 +34,7 @@ for line in file:
     if len(line) == number and line[0]==letter:
         print(line)
 
-
+"""
 file = open('engmix.txt')
 
 
