723abe8bb9a37120fd92c9ebc6288bd46ca418de
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-05-21 08:09:13 -0400

    Updated from Brython Server: 5/21/2018 8:09:14 AM

--- quiz6.py
+++ quiz6.py
@@ -2,6 +2,7 @@
 #5/21/18
 #quiz5.py
 
+"""
 file = open('engmix.txt')
 
 letter = input('What letter would you like to search for?')
@@ -11,3 +12,13 @@ for line in file:
     number = word.count(letter)
     if number == 4:
         print(word)
+"""
+
+file = open('engmix.txt')
+
+for line in file:
+    line = line.strip()
+    if line[0]==line[4]==line[8]:
+        print(line)
+
+
